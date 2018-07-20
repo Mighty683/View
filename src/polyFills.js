48283894649b6c373fe9b9fc4850433a8096e658
Tomasz Szarek <tomasz93szarek@gmail.com>
2018-07-20 12:01:02 +0200

    Add Polyfills

---
+++ src/polyFills.js
@@ -0,0 +1,24 @@
+function assign (target, varArgs) {
+  if (target == null) {
+    throw new TypeError('Cannot convert undefined or null to object');
+  }
+  var to = Object(target)
+
+  for (var index = 1; index < arguments.length; index++) {
+    var nextSource = arguments[index]
+
+    if (nextSource != null) { // Skip over if undefined or null
+      for (var nextKey in nextSource) {
+        // Avoid bugs when hasOwnProperty is shadowed
+        if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+          to[nextKey] = nextSource[nextKey]
+        }
+      }
+    }
+  }
+  return to
+}
+
+module.exports = {
+  assign: assign
+}
